ce9ae3464b72d90ef8086d5ab2c96e1f9893712e
GyeongMo Lim <km9173@naver.com>
2016-05-20 00:54:47 +0900

    Add UserModel Schema and get one User API

---
+++ models/UserModel.js
@@ -0,0 +1,33 @@
+var Sequelize = require('sequelize');
+var sequelize = require('../config/develop');
+
+var User = sequelize.define('user', {
+  userId: {type: Sequelize.STRING, unique: true},
+  password: Sequelize.STRING,
+  exp: Sequelize.BIGINT,
+  image: Sequelize.STRING,
+  grade: Sequelize.INTEGER,
+  name: Sequelize.STRING,
+  userKey: Sequelize.STRING,
+  status: {
+    type: Sequelize.ENUM,
+    values: ['active', 'inactive', 'pending', 'dropped']
+  }
+}, {
+  freezeTableName: true // Model tableName will be the same as the model name
+});
+
+User.sync({force: true}).then(function () {
+  return User.create({
+    userId: 'user1',
+    password: 'password',
+    exp: 10000,
+    image: 'asdf.png',
+    grade: 12,
+    name: 'GyeongMo',
+    userKey: 'asdf',
+    status: 'active'
+  });
+});
+
+module.exports = User;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/UserModel');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/:userId', function(req, res, next) {
+  User.findOne({
+    where: {
+      userId: String(req.params.userId)
+    }}).then(function (user) {
+    if (user != null)
+      res.status(200).json({
+        userId: user.userId,
+        exp: user.exp,
+        image: user.image,
+        grade: user.grade,
+        name: user.name,
+        userKey: user.userKey,
+        status: user.status
+      });
+    else
+      res.status(404).json({
+        message: 'not found user'
+      });
+  });
 });
 
 module.exports = router;
