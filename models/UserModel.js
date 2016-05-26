b46a0a562fbfc43216153bbff99d3b3d6b2d063c
GyeongMo Lim <km9173@naver.com>
2016-05-26 23:37:35 +0900

    Remove code which insert dummy User record

--- models/UserModel.js
+++ models/UserModel.js
@@ -2,7 +2,7 @@ var Sequelize = require('sequelize');
 var sequelize = require('../config/develop');
 
 var User = sequelize.define('user', {
-  userKey: { type: Sequelize.INTEGER, autoIncrement: true , primaryKey: true},
+  userKey: { type: Sequelize.INTEGER, autoIncrement: true, primaryKey: true },
   userId: { type: Sequelize.STRING, unique: true, primaryKey: true },
   password: Sequelize.STRING,
   name: Sequelize.STRING,
@@ -17,17 +17,6 @@ var User = sequelize.define('user', {
   freezeTableName: true // Model tableName will be the same as the model name
 });
 
-User.sync({force: true}).then(function () {
-  return User.create({
-    userKey: 1,
-    userId: 'user1',
-    password: 'password',
-    name: 'GyeongMo',
-    image: 'asdf.png',
-    grade: 12,
-    exp: 10000,
-    status: 'active'
-  });
-});
+User.sync({force: true});
 
 module.exports = User;
