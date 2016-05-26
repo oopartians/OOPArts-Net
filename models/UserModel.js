8c6e6d68141330ba851313960a7b3d9f55cf8139
peloct <peloct@naver.com>
2016-05-26 16:31:52 +0900

    Update UserModel. Creation of favorite, comment

--- models/UserModel.js
+++ models/UserModel.js
@@ -8,7 +8,7 @@ var User = sequelize.define('user', {
   image: Sequelize.STRING,
   grade: Sequelize.INTEGER,
   name: Sequelize.STRING,
-  userKey: Sequelize.STRING,
+  userKey: { type: Sequelize.INTEGER, autoIncrement: true },
   status: {
     type: Sequelize.ENUM,
     values: ['active', 'inactive', 'pending', 'dropped']
@@ -16,6 +16,37 @@ var User = sequelize.define('user', {
 }, {
   freezeTableName: true // Model tableName will be the same as the model name
 });
+
+var Tag;
+
+var Favorite = sequelize.define('favorite', {
+    favoriteKey: { type: Sequelize.INTEGER, autoIncrement: true },
+    name: { type: Sequelize.STRING, validate: { notNull: true } }
+}, {
+    freezeTableName : true
+});
+Favorite.hasOne(User, { foreignKey: 'userKey' });
+
+var Favorite_Tag = sequelize.define('favorite-tag', {
+}, {
+    freezeTableName : true
+});
+Favorite_Tag.hasOne(Favorite, { foreignKey: 'favoriteKey' });
+Favorite_Tag.hasOne(Tag, { foreignKey: 'tagKey' });
+
+var Comment = sequelize.define('comment', {
+    commentKey: { type: Sequelize.INTEGER, autoIncrement: true },
+    createdAt: Sequelize.DATE,
+    comment: Sequelize.STRING,
+    status: {
+        type: Sequelize.ENUM,
+        values: ['normal', 'dropped']
+    }
+}, {
+    freezeTableName : true
+});
+Comment.hasOne(Posting, { foreignKey: 'postingKey' });
+Comment.hasOne(User, { foreignKey: 'userKey' });
 
 User.sync({force: true}).then(function () {
   return User.create({
