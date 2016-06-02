67b78454cf134bbf6fb789a90a6ea01f81487152
YGG <gigyun@naver.com>
2016-06-02 16:16:16 +0900

    still working..create update/delete

--- routes/posting.js
+++ routes/posting.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
 var Posting = require('../models/PostingModel');
+var Tag = require('../models/TagModel');
 
 /* CREATE Posting */
 router.post('/', function (req, res, next) {
     // TODO: check request data validation
     var posting = Posting.build({
         title: req.body.title,
-        content: req.body.
+        content: req.body.content
         //tag: req.body.tag
     });
     posting.save().then(function () {
@@ -42,10 +43,9 @@ router.get('/:postingKey', function(req, res, next) {
 
 /* READ Posting search */
 router.get('/', function(req, res, next) {
-
     Posting.findAll({
         where: req.query,
-        attributes: ['userId', 'name', 'title', 'tag']
+        attributes: ['userId', 'name', 'title']//, 'tag']
     }).then(function (posting) {
         if (posting != null)
             res.status(200).json({
@@ -64,15 +64,15 @@ router.get('/', function(req, res, next) {
     });
 });
 
-/* UPDATE User */
+/* UPDATE Posting */
 router.put('/:postingKey', function(req, res, next) {
     /* TODO: check request data validation
      and, check if query is valid update param key field */
-    User.update(
+    Posting.update(
         req.body,
         {
             where: {
-                userId: req.params.userId
+                postingKey: req.params.postingKey
             },
             limit: 1
         }
@@ -83,20 +83,13 @@ router.put('/:postingKey', function(req, res, next) {
     });
 });
 
-/* DELETE User */
+/* DELETE Posting */
 router.delete('/:postingKey', function(req, res, next) {
     /* TODO: check request data validation */
-    User.update(
-        {
-            status: 'dropped'
-        },
-        {
-            where: {
-                userId: req.params.userId
-            },
-            limit: 1
-        }
-    ).then(function () {
+    Posting.findOne(
+        where: {
+        postingKey: String(req.params.postingKey)
+    }).then(function (posting) {
         res.status(200).json({message: 'success'});
     }).catch(function (error) {
         res.status(400).json({message: error});
