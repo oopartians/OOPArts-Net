8de7c657730e2c120e7a39ce305c9957a449a522
minho <awaeaq@naver.com>
2016-06-02 17:40:10 +0900

    Tag API Version 1.0

--- routes/tag.js
+++ routes/tag.js
@@ -6,27 +6,36 @@ var Tag = require('../models/TagModel');
 router.post('/', function (req, res, next) {
     // TODO: check request data validation
     var tag = Tag.build({
-        name: req.body.name
+        name: req.body.name,
+        role: req.body.role
     });
     tag.save().then(function () {
-        res.status(200).json({
-            message: 'success'
-        });
+        res.status(200).json({ message: 'success' });
     }).catch(function (error) {
-        res.status(400).json({
-            message: 'error'
-        });
+        res.status(400).json({ message: 'error' });
     });
 });
 
-/* Update Tag Name*/
-router.put('/', function(req, res, next) {
-
+/* Update Tag Role*/
+router.put('/:tagKey', function(req, res, next) {
+    Tag.update({ role: req.body.new_role }, {
+        where: { tagKey: req.params.tagKey }, limit: 1}
+    ).then(function () {
+        res.status(200).json({ message: 'success' });
+    }).catch(function (error) {
+        res.status(400).json({ message: error });
+    });
 });
 
 /* Delete Tag */
-router.delete('/', function (req, res, next) {
-
+router.delete('/:name', function (req, res, next) {
+    Tag.destroy({
+        where: { name: req.params.name }
+    }).then(function () {
+        res.status(200).json({ message: 'success' });
+    }).catch(function (error) {
+        res.status(400).json({ message: error });
+    });
 });
 
 /* Search All Tag */
@@ -35,13 +44,9 @@ router.get('/', function(req, res, next) {{
         attribute: ['name']
     }).then(function (tag) {
         if(tag != null)
-            res.status(200).json({
-                Tags: tag
-            });
+            res.status(200).json({ Tags: tag });
         else
-            res.status(400).json({
-                message: 'error'
-            })
+            res.status(400).json({ message: 'error' });
     });
 }})
 
@@ -49,19 +54,16 @@ router.get('/', function(req, res, next) {{
 /* Search One Tag */
 router.get('/:name', function (req, res, next) {
     Tag.findOne({
-        where: {
-            name: String(req.params.name)
-        }
+        where: { name: String(req.params.name) }
     }).then(function (tag) {
         if (tag != null)
             res.status(200).json({
                 name: tag.name,
-                role: tag.role
+                role: tag.role,
+                tagKey: tag.tagKey
             });
         else
-            res.status(404).json({
-                message: 'Not Found'
-            });
+            res.status(404).json({ message: 'Not Found' });
     });
 });
 
