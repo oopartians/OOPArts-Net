da08a31d4e6e47f564cc651157e7af2e13a81b06
GyeongMo Lim <km9173@naver.com>
2016-08-26 00:16:50 +0900

    Change cross domain origin option

--- app/app.js
+++ app/app.js
@@ -59,7 +59,7 @@ var tags = require('./routes/tag');
 var login = require('./routes/login');
 
 var allowCrossDomain = function (req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'http://ooparts.club');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
 
